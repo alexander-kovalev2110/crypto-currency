79889be5df9a8a937183061e83aab4860de4c716
alexander-kovalev2110 <alexander.kovalev@yahoo.com>
2023-06-27 20:15:13 +0300

    package.json

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
       <StyledEngineProvider injectFirst>
           <App />
       </StyledEngineProvider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
